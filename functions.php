72f9e6450aadc41b61e0ddd5de98cc85c6741a62
Tuhin Khan <uiu.tuhin@gmail.com>
2022-02-22 23:48:30 +0600

    script file issue solved

--- functions.php
+++ functions.php
@@ -21,10 +21,10 @@ function philoshopy_assets(){
 
     //theme js file added
     wp_enqueue_script('jquery');
-    wp_enqueue_script('modernizr', get_template_directory_uri().'/assets/js/modernizr.js', array(),'4.0.2','true');
-    wp_enqueue_script('pace', get_template_directory_uri().'/assets/js/pace.min.js', array(),'4.0.2','true');
-    wp_enqueue_script('jsplugins', get_template_directory_uri().'/assets/js/jsplugins.js', array(),'4.0.2','true');
-    wp_enqueue_script('jsmain', get_template_directory_uri().'/assets/js/jsmain.js', array(),'4.0.2','true');
+    wp_enqueue_script('modernizr', get_template_directory_uri().'/assets/js/modernizr.js', array(),'false','true');
+    wp_enqueue_script('pace', get_template_directory_uri().'/assets/js/pace.min.js', array(),'false','true');
+    wp_enqueue_script('jsplugins', get_template_directory_uri().'/assets/js/jsplugins.js', array(),'false','true');
+    wp_enqueue_script('jsmain', get_template_directory_uri().'/assets/js/jsmain.js', array(),'false','true');
 }
 
 add_action('wp_enqueue_scripts','philoshopy_assets');
